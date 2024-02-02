cde68ac3ebe24a0955960c8b44fd9f5c9337f5f3
Abhay Singh Rawat <129993139+Abhaysinghrawat@users.noreply.github.com>
2024-02-03 00:55:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const api = require("./src/api");
+
+const app = express();
+const PORT = 4200;
+
+app.use(express.json());
+app.use("/api", api);
+app.get("/", (req, res) => {
+  res.send("<h1>Hello from Abhay</h1>");
+});
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
